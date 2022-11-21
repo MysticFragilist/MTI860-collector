6a880890df327bb4eeeb6642840c447da37a628e
Samuel Montambault <mont.samuel@outlook.com>
2022-11-20 20:06:45 -0500

    test out noew fix

--- Collect.cs
+++ Collect.cs
@@ -22,10 +22,9 @@ namespace MTI860_collector
         [FunctionName("collect")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "username" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Username** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
             [CosmosDB(databaseName: "vr-data", collectionName: "collect-data",
             ConnectionStringSetting = "CosmosDbConnectionString"
             )]IAsyncCollector<dynamic> documentsOut,
--- Models/CollectRequest.cs
+++ Models/CollectRequest.cs
@@ -1,42 +1,50 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 
-namespace MTI860_collector.Models;
-
-public class CollectRequest
+namespace MTI860_collector.Models
 {
+    public class CollectRequest
+    {
 
-    [JsonProperty("username")]
-    public string Username { get; set; }
-    
-    [JsonProperty("laser")]
-    public MethodCollect Laser { get; set; }
-    
-    [JsonProperty("virtualPen")]
-    public MethodCollect VirtualPen { get; set; }
-    
-    [JsonProperty("handTracking")]
-    public MethodCollect HandTracking { get; set; }
-}
+        [JsonProperty("username")]
+        public string Username { get; set; }
 
-public class MethodCollect
-{
+        [JsonProperty("laser")]
+        public MethodCollect Laser { get; set; }
 
-    [JsonProperty("circles")]
-    public IEnumerable<ShapeData> Circles { get; set; }
+        [JsonProperty("virtualPen")]
+        public MethodCollect VirtualPen { get; set; }
 
-    [JsonProperty("triangles")]
-    public IEnumerable<ShapeData> Triangles { get; set; }
+        [JsonProperty("handTracking")]
+        public MethodCollect HandTracking { get; set; }
+    }
 
-    [JsonProperty("stars")]
-    public IEnumerable<ShapeData> Stars { get; set; }
-}
+    public class MethodCollect
+    {
+
+        [JsonProperty("circles")]
+        public IEnumerable<ShapeData> Circles { get; set; }
+
+        [JsonProperty("triangles")]
+        public IEnumerable<ShapeData> Triangles { get; set; }
+
+        [JsonProperty("stars")]
+        public IEnumerable<ShapeData> Stars { get; set; }
+    }
 
 
-public class ShapeData
-{
-    public double ScoreCircle { get; set; }
-    public double ScoreTriangle { get; set; }
-    public double ScoreStar { get; set; }
-    public double Time { get; set; }
+    public class ShapeData
+    {
+        [JsonProperty("scoreCircle")]
+        public double ScoreCircle { get; set; }
+
+        [JsonProperty("scoreTriangle")]
+        public double ScoreTriangle { get; set; }
+
+        [JsonProperty("scoreStar")]
+        public double ScoreStar { get; set; }
+
+        [JsonProperty("time")]
+        public double Time { get; set; }
+    }
 }
